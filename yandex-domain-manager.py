a8f1cb8e101840601f5ac1d5b4dad383657c2543
Viktor Sidochenko <viktor.sidochenko@gmail.com>
2017-02-22 14:55:46 +0700

    fix formatting

--- yandex-domain-manager.py
+++ yandex-domain-manager.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
+
 import json
 import os
-import requests
-
 import argparse
+import requests
 
 base_url = 'https://pddimp.yandex.ru/api2/admin'
 
-parser = argparse.ArgumentParser(description='Domain manager for http://pdd.yandex.ru service')
+parser = argparse.ArgumentParser(
+    description='Domain manager for http://pdd.yandex.ru service')
 parser.add_argument('action', help='add or remove')
 parser.add_argument('domain', help='domain name')
 parser.add_argument('--ip', help='Domain ip')
-parser.add_argument('--ttl', default=360, help='Ttl dor domain. default is 360 seconds')
+parser.add_argument('--ttl', default=360,
+                    help='Ttl dor domain. default is 360 seconds')
 parser.add_argument('--token', default=os.environ.get('PDD_TOKEN'),
                     help='api access token, $PDD_TOKEN environment variable can be used')
 
@@ -26,7 +28,7 @@ def main():
         exit(1)
 
     if args.token is None:
-        print ('Api token not defined')
+        print 'Api token not defined'
         parser.parse_args(['-h'])
         exit(1)
 
@@ -37,7 +39,7 @@ def main():
 
     if args.action == 'add':
         if args.ip is None:
-            print ('Ip not set')
+            print 'Ip not set'
             parser.parse_args(['-h'])
             exit(2)
         add(domain, subdomain, args.ttl, args.ip)
@@ -46,22 +48,23 @@ def main():
 
 
 def add(domain_name, subdomain_name, ttl, ip):
-    params = {'domain': domain_name, 'type': 'A', 'subdomain': subdomain_name, 'ttl': ttl, 'content': ip}
+    params = {'domain': domain_name, 'type': 'A',
+              'subdomain': subdomain_name, 'ttl': ttl, 'content': ip}
     end_point = base_url + '/dns/add'
     response = requests.post(end_point,
                              headers=request_headers, params=params)
 
     if response.status_code not in range(200, 300) or (
-                    json.loads(response.text)['success'] == 'error' and
-                    json.loads(response.text)['error'] != 'record_exists'):
-        print ('Failed to add domain: ' + response.text)
+            json.loads(response.text)['success'] == 'error' and
+            json.loads(response.text)['error'] != 'record_exists'):
+        print 'Failed to add domain: ' + response.text
         exit(2)
 
 
 def remove(domain_name, fqdn):
     record_id = __get_record_id(domain_name, fqdn)
     if record_id is None:
-        print ('No such domain')
+        print 'No such domain'
         exit(0)
     params = {'domain': domain_name, 'record_id': record_id}
     end_point = base_url + '/dns/del'
@@ -69,9 +72,9 @@ def remove(domain_name, fqdn):
                              headers=request_headers, params=params)
 
     if response.status_code not in range(200, 300) or (
-                    json.loads(response.text)['success'] == 'error' and
-                    json.loads(response.text)['error'] != 'no_such_record'):
-        print ('Failed to remove domain: ' + response.text)
+            json.loads(response.text)['success'] == 'error' and
+            json.loads(response.text)['error'] != 'no_such_record'):
+        print 'Failed to remove domain: ' + response.text
         exit(2)
 
 
@@ -79,8 +82,9 @@ def __get_record_id(domain_name, fqdn):
     end_point = base_url + '/dns/list?domain=' + domain_name
     response = requests.get(end_point, headers=request_headers)
 
-    if response.status_code not in range(200, 300) or json.loads(response.text)['success'] == 'error':
-        print (response.text)
+    if response.status_code not in range(200, 300) \
+            or json.loads(response.text)['success'] == 'error':
+        print response.text
         exit(2)
 
     records = json.loads(response.text)['records']
